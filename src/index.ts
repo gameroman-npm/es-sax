3b6d2cbb4460a3c809613f5219f9a0960ab89438
Roman <dev@rman.dev>
2026-06-01 22:30:45 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -83,6 +83,39 @@ function getDeclaredEncoding(body: string): string | null | undefined {
   return match ? match[2] : null;
 }
 
+function charAt(chunk: string, i: number): string {
+  let result = "";
+  if (i < chunk.length) {
+    result = chunk.charAt(i);
+  }
+  return result;
+}
+
+// this really needs to be replaced with character classes.
+// XML allows all manner of ridiculous numbers and digits.
+const CDATA = "[CDATA[";
+const DOCTYPE = "DOCTYPE";
+const XML_NAMESPACE = "http://www.w3.org/XML/1998/namespace";
+const XMLNS_NAMESPACE = "http://www.w3.org/2000/xmlns/";
+const rootNS = { xml: XML_NAMESPACE, xmlns: XMLNS_NAMESPACE };
+
+// http://www.w3.org/TR/REC-xml/#NT-NameStartChar
+// This implementation works on strings, a single character at a time
+// as such, it cannot ever support astral-plane characters (10000-EFFFF)
+// without a significant breaking change to either this  parser, or the
+// JavaScript language.  Implementation of an emoji-capable xml parser
+// is left as an exercise for the reader.
+const nameStart =
+  /[:_A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]/;
+
+const nameBody =
+  /[:_A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u00B7\u0300-\u036F\u203F-\u2040.\d-]/;
+
+const entityStart =
+  /[#:_A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]/;
+const entityBody =
+  /[#:_A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u00B7\u0300-\u036F\u203F-\u2040.\d-]/;
+
 const sax: unknown = {};
 
 (function (sax) {
@@ -431,31 +464,6 @@ const sax: unknown = {};
 
     return Stream.prototype.on.call(me, ev, handler);
   };
-
-  // this really needs to be replaced with character classes.
-  // XML allows all manner of ridiculous numbers and digits.
-  const CDATA = "[CDATA[";
-  const DOCTYPE = "DOCTYPE";
-  const XML_NAMESPACE = "http://www.w3.org/XML/1998/namespace";
-  const XMLNS_NAMESPACE = "http://www.w3.org/2000/xmlns/";
-  const rootNS = { xml: XML_NAMESPACE, xmlns: XMLNS_NAMESPACE };
-
-  // http://www.w3.org/TR/REC-xml/#NT-NameStartChar
-  // This implementation works on strings, a single character at a time
-  // as such, it cannot ever support astral-plane characters (10000-EFFFF)
-  // without a significant breaking change to either this  parser, or the
-  // JavaScript language.  Implementation of an emoji-capable xml parser
-  // is left as an exercise for the reader.
-  const nameStart =
-    /[:_A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]/;
-
-  const nameBody =
-    /[:_A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u00B7\u0300-\u036F\u203F-\u2040.\d-]/;
-
-  const entityStart =
-    /[#:_A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]/;
-  const entityBody =
-    /[#:_A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u00B7\u0300-\u036F\u203F-\u2040.\d-]/;
 
   let S = 0;
   sax.STATE = {
@@ -1174,14 +1182,6 @@ const sax: unknown = {};
       parser.textNode = c;
       parser.state = S.TEXT;
     }
-  }
-
-  function charAt(chunk, i) {
-    let result = "";
-    if (i < chunk.length) {
-      result = chunk.charAt(i);
-    }
-    return result;
   }
 
   function write(chunk) {
