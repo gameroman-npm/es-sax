dadc22cd2099d03f6ace41d5da227bca375f9d14
Roman <dev@rman.dev>
2026-06-01 22:04:33 +0100

    Update parser-position.test.ts

--- tests/parser-position.test.ts
+++ tests/parser-position.test.ts
@@ -1,39 +1,44 @@
+import assert from "node:assert";
+import { test } from "node:test";
+
 import sax from "es-sax";
-
-import { test } from "./index.ts";
-
-var tap = require("tap");
 
 function testPosition(chunks, expectedEvents) {
   var parser = sax.parser();
+
   expectedEvents.forEach(function (expectation) {
     parser["on" + expectation[0]] = function () {
       for (var prop in expectation[1]) {
-        tap.equal(parser[prop], expectation[1][prop]);
+        assert.strictEqual(parser[prop], expectation[1][prop]);
       }
     };
   });
+
   chunks.forEach(function (chunk) {
     parser.write(chunk);
   });
 }
 
-testPosition(
-  ["<div>abcdefgh</div>"],
-  [
-    ["opentagstart", { position: 5, startTagPosition: 1 }],
-    ["opentag", { position: 5, startTagPosition: 1 }],
-    ["text", { position: 19, startTagPosition: 14 }],
-    ["closetag", { position: 19, startTagPosition: 14 }],
-  ],
-);
+test("should track positions for a single chunk", function () {
+  testPosition(
+    ["<div>abcdefgh</div>"],
+    [
+      ["opentagstart", { position: 5, startTagPosition: 1 }],
+      ["opentag", { position: 5, startTagPosition: 1 }],
+      ["text", { position: 19, startTagPosition: 14 }],
+      ["closetag", { position: 19, startTagPosition: 14 }],
+    ],
+  );
+});
 
-testPosition(
-  ["<div>abcde", "fgh</div>"],
-  [
-    ["opentagstart", { position: 5, startTagPosition: 1 }],
-    ["opentag", { position: 5, startTagPosition: 1 }],
-    ["text", { position: 19, startTagPosition: 14 }],
-    ["closetag", { position: 19, startTagPosition: 14 }],
-  ],
-);
+test("should track positions across split chunks", function () {
+  testPosition(
+    ["<div>abcde", "fgh</div>"],
+    [
+      ["opentagstart", { position: 5, startTagPosition: 1 }],
+      ["opentag", { position: 5, startTagPosition: 1 }],
+      ["text", { position: 19, startTagPosition: 14 }],
+      ["closetag", { position: 19, startTagPosition: 14 }],
+    ],
+  );
+});
