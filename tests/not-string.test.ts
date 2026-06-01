0cefe15153e59f3c297ed103dd6c58787f44dab4
Roman <dev@rman.dev>
2026-06-01 21:45:00 +0100

    Update not-string.test.ts

--- tests/not-string.test.ts
+++ tests/not-string.test.ts
@@ -1,8 +1,18 @@
-var parser = require("../").parser(true);
-var t = require("tap");
-t.plan(1);
-parser.onopentag = function (node) {
-  t.same(node, { name: "x", attributes: {}, isSelfClosing: false });
-};
-var xml = Buffer.from("<x>y</x>");
-parser.write(xml).close();
+import { test } from "node:test";
+import assert from "node:assert";
+import sax from "es-sax";
+
+test("should parse buffer input correctly", () => {
+  var parser = sax.parser(true);
+
+  parser.onopentag = function (node) {
+    assert.deepStrictEqual(node, {
+      name: "x",
+      attributes: {},
+      isSelfClosing: false,
+    });
+  };
+
+  var xml = Buffer.from("<x>y</x>");
+  parser.write(xml).close();
+});
